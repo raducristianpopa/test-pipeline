e058ed1dc8e39882eb7b254277f3fbd2cb3c4a55
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-08-20 14:19:28 +0300

    Test action

---
+++ .github/actions/test-action.cjs
@@ -0,0 +1,28 @@
+// @ts-check
+
+/**
+ * 1. Validate input version.
+ * 2. Check if given tag/release is already promoted to stable. If so, crash.
+ * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
+ */
+module.exports = async ({ github, context }) => {
+  const { owner, repo } = context.repo
+  const previewVersionTag = process.env.INPUT_VERSION
+  if (!previewVersionTag) {
+    throw new Error('Missing env.INPUT_VERSION')
+  }
+  if (!previewVersionTag.match(/^v[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+-preview$/)) {
+    throw new Error('Input "version" must match vX.X.X.X-preview')
+  }
+
+  const versionTag = previewVersionTag.replace('-preview', '')
+  const res = await github.rest.repos.getReleaseByTag({
+    owner,
+    repo,
+    tag: versionTag
+  })
+  if (res.status < 400) {
+    throw new Error('Release already promoted to stable')
+  }
+}
+
